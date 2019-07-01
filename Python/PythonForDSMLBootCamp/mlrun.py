98d627d5e644afbe153c1ce804e197b53ffd0a58
stanworld <songtancs@gmail.com>
2019-06-30 21:26:32 -0400

    capstone exercise

--- Python/PythonForDSMLBootCamp/mlrun.py
+++ Python/PythonForDSMLBootCamp/mlrun.py
@@ -10,10 +10,38 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df=pd.read_csv("/home/stan/Downloads/Refactored_Py_DS_ML_Bootcamp-master/11-Linear-Regression/USA_Housing.csv")
-df.info()
-df.describe()
-df.columns
+# df=pd.read_csv("/home/stan/Downloads/Refactored_Py_DS_ML_Bootcamp-master/11-Linear-Regression/USA_Housing.csv")
+# df.info()
+# df.describe()
+# df.columns
 
-x=sns.pairplot(df)
-x.savefig("output.png")
+#x=sns.pairplot(df)
+#x.savefig("output.png")
+
+#x=sns.distplot(df['Price'])
+#plt.show()
+
+
+# X = df[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+#                'Avg. Area Number of Bedrooms', 'Area Population']]
+# y = df['Price']
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+
+from sklearn.linear_model import LinearRegression
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+
+predictions = lm.predict(X_test)
+#plt.scatter(y_test,predictions)
+
+#sns.distplot((y_test-predictions),bins=50)
+#plt.show()
+
+# logistic regression : for classification
+train=pd.read_csv("/home/stan/Downloads/Refactored_Py_DS_ML_Bootcamp-master/13-Logistic-Regression/titanic_train.csv")
+# use heatmap to find out which part of data is missing most
+sns.heatmap(train.isnull(),yticklabels=False,cbar=False, cmap="viridis")
+plt.show()
