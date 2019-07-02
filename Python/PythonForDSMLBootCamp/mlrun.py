1c736db73ed5eb57581e0351b258532e1783bfef
stanworld <songtancs@gmail.com>
2019-07-01 21:08:47 -0400

    capstone exercise

--- Python/PythonForDSMLBootCamp/mlrun.py
+++ Python/PythonForDSMLBootCamp/mlrun.py
@@ -45,3 +45,56 @@ train=pd.read_csv("/home/stan/Downloads/Refactored_Py_DS_ML_Bootcamp-master/13-L
 # use heatmap to find out which part of data is missing most
 sns.heatmap(train.isnull(),yticklabels=False,cbar=False, cmap="viridis")
 plt.show()
+# data preprocessing
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+
+    if pd.isnull(Age):
+
+        if Pclass == 1:
+            return 37
+
+        elif Pclass == 2:
+            return 29
+
+        else:
+            return 24
+
+    else:
+        return Age
+
+# replace age with median of each Pclass
+train['Age'] = train[['Age','Pclass']].apply(impute_age,axis=1)
+# since there are too many missing values in Cabin, just drop the column directly
+train.drop('Cabin',axis=1,inplace=True)
+# drop a row still with missing values
+train.dropna(inplace=True)
+
+# Convert Categorical features
+
+sex = pd.get_dummies(train['Sex'],drop_first=True)
+embark = pd.get_dummies(train['Embarked'],drop_first=True)
+
+train.drop(['Sex','Embarked','Name','Ticket','PassengerId'],axis=1,inplace=True)
+
+train = pd.concat([train,sex,embark],axis=1)
+
+X = train.drop('Survived',axis=1)
+y = train['Survived']
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import classification_report
+
+X_train, X_test, y_train, y_test = train_test_split(train.drop('Survived',axis=1),
+                                                    train['Survived'], test_size=0.30,
+                                                    random_state=101)
+
+logmodel = LogisticRegression()
+logmodel.fit(X_train,y_train)
+
+predictions = logmodel.predict(X_test)
+print(classification_report(y_test,predictions))
